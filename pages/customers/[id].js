1dfd3bf05be7e8af5cfe219a4405338ebf00637f
grit-n-grams <grits.n.grams@gmail.com>
2022-08-12 13:05:37 +1200

    set up customer profile routes

---
+++ pages/customers/[id].js
@@ -0,0 +1,5 @@
+// individual customers profile add tranactions
+// call createTransactions api
+export default function profile() {
+  return <h1>Customer Profile Page</h1>
+}
---
+++ pages/customers/index.js
@@ -0,0 +1,6 @@
+// fetch a list of customers and have search bar
+// call getAll api
+
+export default function customerSearch() {
+  return <h1>Customer search</h1>
+}
